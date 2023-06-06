01d6910dd5dca35a94cc904c035c21f775ff90dd
suweathan <134991749+suweathan@users.noreply.github.com>
2023-06-06 08:00:11 +0530

    Add files via upload

--- suweathan page 02.py
+++ suweathan page 02.py
@@ -1,3 +1,11 @@
-print('Hello, World')
-person=input('What is your name?')
-print('Hello',person)
+import turtle
+
+def draw_circle(size):
+    turtle.pencolor('red')
+    turtle.circle(size)
+    draw_circle(size)
+
+turtle.bgcolor('black')
+turtle.speed('fast')
+turtle.pensize(4)
+draw_circle(30)
